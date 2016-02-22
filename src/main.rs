c82aaa8b056be4ca9615c5ca5b3a7f19a5553b6b
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-02-22 15:40:15 -0600

    Attempt at creating clients

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::channel;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    // accept connections and process them, spawning a new thread for each one
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    // connection succeeded
+                    create_client(stream);
+                });
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
+
+    // close the socket server
+    drop(listener);
+}
+
+fn create_client(stream: TcpStream) {
+    println!("New client");
+    let mut stream = stream;
+    stream.write(&"Welcome to Smazy".to_string().into_bytes()).unwrap();
+    stream.write(&"Please enter a username".to_string().into_bytes()).unwrap();
+    stream.flush();
+    let mut username = "".to_string();
+    stream.read_to_string(&mut username).unwrap();
+    println!("Username is {}", username);
 }
