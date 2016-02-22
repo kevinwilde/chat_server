f85f6dfbe54116da8aecadcb21629ffae6dd5638
kevinwilde <kevinwilde2018@u.northwestern.edu>
2016-02-22 16:04:36 -0600

    Check for username in hashset before allowing client to use it

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use std::collections::HashSet;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::sync::mpsc::channel;
 use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let usernames = Arc::new(Mutex::new(HashSet::new()));
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let usernames = usernames.clone();
                 thread::spawn(move|| {
                     // connection succeeded
-                    create_client(stream);
+                    create_client(stream, &usernames);
                 });
             }
             Err(e) => {
@@ -25,7 +29,7 @@ fn main() {
     drop(listener);
 }
 
-fn create_client(stream: TcpStream) {
+fn create_client(stream: TcpStream, usernames: &Arc<Mutex<HashSet<String>>>) {
     println!("New client");
     let mut stream = stream;
     stream.write(&"Welcome to Smazy\n".to_string().into_bytes()).unwrap();
@@ -33,6 +37,14 @@ fn create_client(stream: TcpStream) {
     stream.flush().unwrap();
     let mut reader = BufReader::new(stream);
     let mut username = "".to_string();
-    reader.read_line(&mut username).unwrap();
-    println!("Username is {}", username);
+    match reader.read_line(&mut username) {
+        Ok(n) => {
+            let mut guard = usernames.lock().unwrap();
+            if n > 0 && !guard.contains(&username) {
+                guard.insert(username.to_string());
+                println!("Username is {}", username.to_string());
+            }
+        },
+        Err(e) => println!("{}", e)
+    }
 }
