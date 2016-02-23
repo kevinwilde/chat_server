3b1bcb0e52f9692d50355c63474f69f1f793745c
kevinwilde <kevinwilde2018@u.northwestern.edu>
2016-02-23 10:48:18 -0600

    Getters for message struct

--- src/message.rs
+++ src/message.rs
@@ -1,6 +1,33 @@
 pub struct Message {
-    pub date: String,
-    pub from: String,
-    pub to: String,
-    pub content: String
+    date: String,
+    from: String,
+    to: String,
+    content: String
+}
+
+impl Message {
+    pub fn new(date: String, from: String, to: String, content: String) -> Message {
+        Message {
+            date: date,
+            from: from,
+            to: to,
+            content: content
+        }
+    }
+
+    pub fn get_date(&self) -> String {
+        (*self.date).to_string()
+    }
+
+    pub fn get_from(&self) -> String {
+        (*self.from).to_string()
+    }
+
+    pub fn get_to(&self) -> String {
+        (*self.to).to_string()
+    }
+
+    pub fn get_content(&self) -> String {
+        (*self.content).to_string()
+    }
 }
